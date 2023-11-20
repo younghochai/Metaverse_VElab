233082afa0d9cccd0ad1fd284447895eb3b33134
puls36 <puls36@cau.ac.kr>
2023-11-20 09:43:45 +0900

    Update GenDataLoad_smpl.cs

--- Metaverse/Avatar motion/GenDataLoad_smpl.cs
+++ Metaverse/Avatar motion/GenDataLoad_smpl.cs
@@ -662,7 +662,35 @@ public class GenDataLoad : MonoBehaviour
 
 
 
+            for (int i = 1; i < _bodyJointNames.Length - 3; i++)
+            {
+                if (i == 5)
+                    continue;
+                else if (i == 4)
+                    continue;
+                else if (i == 7)
+                    continue;
 
+                else if (i == 8)
+                    continue;
+                else if (i == 1)
+                    continue;
+                else if (i == 2)
+                    continue;
+                else if (i == 12)
+                    continue;
+                else if (i == 15)
+                    continue;
+
+                else if (i == 10)
+                    continue;
+
+                else if (i == 11)
+                    continue;
+
+                smpl_manager.SetLocalJointRotation(_bodyJointNames[i], gen_quat_list[i][frame_cnt]);
+
+            }
 
 
             smpl_manager.UpdateJointPositions(false);
